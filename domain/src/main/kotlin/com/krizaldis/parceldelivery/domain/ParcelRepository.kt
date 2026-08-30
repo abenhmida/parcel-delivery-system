e6d39104941c53909f7caced7c93454fea7e552e
abenhmida <aymen.benhmida@gmail.com>
2026-08-30 19:40:41 +0200

    Add new method update

--- domain/src/main/kotlin/com/krizaldis/parceldelivery/domain/ParcelRepository.kt
+++ domain/src/main/kotlin/com/krizaldis/parceldelivery/domain/ParcelRepository.kt
@@ -5,6 +5,8 @@ import java.util.UUID
 interface ParcelRepository {
     fun save(parcel: Parcel)
 
+    fun update(parcel: Parcel)
+
     fun findById(id: UUID): Parcel?
 
     fun findByTrackingNumber(trackingNumber: String): Parcel?
